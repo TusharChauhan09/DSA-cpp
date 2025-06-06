42670fba8e88c3eae9a55f8d2ba46c2bc6a5ddd2
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-06-06 21:51:31 +0530

    6 june

--- Stacks/Question Part-2/Q6_Stock_Span.cpp
+++ Stacks/Question Part-2/Q6_Stock_Span.cpp
@@ -5,27 +5,46 @@
 #include<algorithm>
 using namespace std;
 
+// vector<int> Stock_Span(vector<int> &given){
+//     reverse(given.begin(),given.end());
+//     vector<int>result(given.size(),1); // OR -1
+//     stack<int>s;
+
+//     s.push(0);
+//     for(int i=0;i<given.size();i++){
+//         while(!s.empty() && given[s.top()]<given[i]){
+//             result[s.top()] = i-s.top();
+//             s.pop();
+//         }
+//         s.push(i);
+//     }
+
+//     reverse(result.begin(),result.end());
+//     return result;
+// }
+
 vector<int> Stock_Span(vector<int> &given){
-    reverse(given.begin(),given.end());
-    vector<int>result(given.size(),1); // OR -1
-    stack<int>s;
-
-    s.push(0);
-    for(int i=0;i<given.size();i++){
-        while(!s.empty() && given[s.top()]<given[i]){
-            result[s.top()] = i-s.top();
-            s.pop();
+    int n = given.size();
+    vector<int>ans(n);  // stores the index cur - index of prev greater 
+    stack<int>st; // stores the index 
+    for(int i=0;i<n;i++){
+        while(!st.empty() && given[st.top()]<=given[i]){
+            st.pop();
         }
-        s.push(i);
+        if(st.empty()){
+            ans[i] = i+1;
+        }
+        else{
+            ans[i] = i-st.top();    
+        }
+        st.push(i);
     }
-
-    reverse(result.begin(),result.end());
-    return result;
+    return ans;
 }
 
 int main(){
     
-    vector<int>given = {100,80,60,70,60,75,85};
+    vector<int>given = {100,80,60,81,70,60,75,85};
 
     vector<int>result = Stock_Span(given);
 
